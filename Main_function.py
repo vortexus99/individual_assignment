febb93b37c38292635960a4b7d8ac35ccfb50204
Vortexus <tharsisplateau@gmail.com>
2019-01-17 16:52:18 +0100

    csv and main function to produce

--- Main_function.py
+++ Main_function.py
@@ -5,7 +5,46 @@ import json
 
 turkey_kurd_raw = [] #init list
 
-with open('turkey_kurdistan.json') as file:
+with open('turkey_kurdistan.json') as file: #write to list
     turkey_kurd_raw = json.load(file)
 
-print(type(turkey_kurd_raw[1]))
+select_vars = ["year","country","side_a","side_b","deaths_a","deaths_b"] #add select_vars/select variables
+
+
+turkey_kurd_filt = [] #init list for filtered json
+
+
+for incident in turkey_kurd_raw: #copy key:value pairs with key in header to filt.json
+
+    #Create a temporay dict, add keys and values
+    temp = {} 
+    for header in select_vars:
+        temp[header] = incident[header]
+
+    #Add temp as new item to filt
+    turkey_kurd_filt.append(temp)
+
+
+#Checking for countries other that turkey
+
+counter = 0 #int counter to return no. countries
+
+#run filtered to print and count
+for incident in turkey_kurd_filt:
+    if incident['country'] != ('Turkey' or 'Iraq'):
+        print(incident['country'])
+        counter += 1
+print(counter)
+
+
+'''
+with open('kurd_kurdistan.csv', 'w') as file:
+ 
+    writer = csv.DictWriter(file, fieldnames=select_vars, lineterminator='\n', delimiter=',')
+    writer.writeheader()
+
+    for incident in turkey_kurd_filt:
+        writer.writerow(incident)
+
+print('I think that worked?')
+'''
